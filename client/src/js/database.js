e7a415ca6949435deb1822e1ef1f99d3d77b397a
yaneyalvarado <alvyaney@gmail.com>
2023-07-06 14:01:58 -0600

    updated database.js by completing the to do's. I added logic that accepts the content and also connects and adds it to the database.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,43 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error("putDb not implemented");
+  // a connection to the databse
+  const contactDb = await openDB("jate", 1);
+
+  // new transaction
+  const tx = contactDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log(result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error("getDb not implemented");
+
+  const contactDb = await openDB("jate", 1);
+  const tx = contactDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  return result?.value;
+};
 
 initdb();
